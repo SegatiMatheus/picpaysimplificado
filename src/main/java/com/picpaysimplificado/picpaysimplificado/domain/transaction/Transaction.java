079f335a397adb615c90ecacdd7270e9f166f35e
Matheus Segati <matheusegati05@gmail.com>
2024-08-22 13:07:53 -0300

    feat: Create Transaction Entity

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,34 @@
+package com.picpaysimplificado.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transaction")
+@Table(name = "transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
